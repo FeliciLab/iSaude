a2e586521e3ebe128493a3f1c261899a4a1da1f1
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-07 17:48:24 -0300

    add dist

---
+++ dist/precache-manifest.d1202011664b4b9f1c4ca905580198cc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1eb9c1cc15557be9a57",
+    "url": "/css/app.526df8db.css"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/fonts/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/fonts/OpenSans-ExtraBold.fb7e3a29.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/fonts/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/fonts/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "bc49f1e8967de5ae0f575a30e0d68052",
+    "url": "/img/doctor.bc49f1e8.svg"
+  },
+  {
+    "revision": "6ee20c5f317cdf1ed33a634fc29a5f76",
+    "url": "/img/esp_branca.6ee20c5f.png"
+  },
+  {
+    "revision": "8e7378402f095e9bf50c649cf26e8e08",
+    "url": "/img/respirator.8e737840.svg"
+  },
+  {
+    "revision": "2587c2e759e479da6d5daa8fa4946782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1eb9c1cc15557be9a57",
+    "url": "/js/app.ec7a5c33.js"
+  },
+  {
+    "revision": "b6ccddf8c165e22dcd42",
+    "url": "/js/chunk-vendors.65c47f23.js"
+  },
+  {
+    "revision": "906355315d54396dcc29fe66bfa2c8f3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "6c05652f351eef3506965fa451fc5310",
+    "url": "/manisfest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
