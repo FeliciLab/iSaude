a04839bffd4fe9b66e9ce0fad20a5575856d6d70
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-07 17:32:29 -0300

    add Vue pwa

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js'
+      swSrc: 'src/registerServiceWorker.js'
       // ...other Workbox options...
     }
   }
