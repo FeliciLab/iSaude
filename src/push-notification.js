fc4a987457954209348d746afb6a56627da4ef80
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-08 16:16:21 -0300

    trade InitializeApp

--- src/push-notification.js
+++ src/push-notification.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase'
 const initFirebase = () => {
   firebase.initializeApp({
-    messagingSenderId: '145552152707'
+    apiKey: 'AIzaSyDcZZrVte2c4zd0jTpnPrhZOIw4KFDkN8A',
+    authDomain: 'covid-19-ceara-7f898.firebaseapp.com',
+    databaseURL: 'https://covid-19-ceara-7f898.firebaseio.com',
+    projectId: 'covid-19-ceara-7f898',
+    storageBucket: 'covid-19-ceara-7f898.appspot.com',
+    messagingSenderId: '145552152707',
+    appId: '1:145552152707:web:4053b6b6a90701dd7be3b6',
+    measurementId: 'G-YDRYSE60EK'
   })
 }
 
