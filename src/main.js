d67e8189fc8b88607258e3ca7aeb8061b677ae65
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-08 16:13:34 -0300

    change order

--- src/main.js
+++ src/main.js
@@ -7,12 +7,10 @@ import './sass/main.scss'
 import './plugins/vue-bootstrap'
 import { initFirebase, requestPushPermission } from './push-notification'
 Vue.config.productionTip = false
-
+initFirebase()
+requestPushPermission()
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
-
-initFirebase()
-requestPushPermission()
