f39f895af97e08119c36fbab69c860af3d383556
Paulo italo Rodrigues <pauloitalo@Mac-de-Paulo.local>
2020-04-15 11:46:11 -0700

    merge

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,13 @@
 </template>
 <script>
 import Header from './components/Header'
-<<<<<<< HEAD
 // import { requestPushPermission } from './push-notification'
-=======
-import { Plugins } from '@capacitor/core'
->>>>>>> a6635381479dc1a018bfb4570f88e8afa3992a5f
 export default {
   components: {
     Header
   },
   mounted () {
-<<<<<<< HEAD
     // requestPushPermission()
-=======
-    const { PushNotifications } = Plugins
-    console.log('Plugins -> ', Plugins)
-    console.log('Push Notification -> ', PushNotifications)
-    PushNotifications.register()
-
-    PushNotifications.addListener('registration', token => {
-      alert('Push registration success, token: ' + token.value)
-      console.log('Push registration success, token: ' + token.value)
-    })
-
-    PushNotifications.addListener('registrationError', error => {
-      alert('Error on registration: ' + JSON.stringify(error))
-      console.log('Error on registration: ' + JSON.stringify(error))
-    })
-
-    PushNotifications.addListener('pushNotificationReceived', notification => {
-      alert('Push received: ' + JSON.stringify(notification))
-      console.log('Error on registration: ' + JSON.stringify(notification))
-    })
-
-    PushNotifications.addListener('pushNotificationActionPerformed', notification => {
-      alert('Push action performed: ' + JSON.stringify(notification))
-      console.log('Error on registration: ' + JSON.stringify(notification))
-    })
->>>>>>> a6635381479dc1a018bfb4570f88e8afa3992a5f
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,14 +5,8 @@ import router from './router'
 import store from './store'
 import './sass/main.scss'
 import './plugins/vue-bootstrap'
-<<<<<<< HEAD
 // import { initFirebase } from './push-notification'
 Vue.config.productionTip = false
-=======
-// import { initFirebase, requestPushPermission } from './push-notification'
-Vue.config.productionTip = false
-// requestPushPermission()
->>>>>>> a6635381479dc1a018bfb4570f88e8afa3992a5f
 // initFirebase()
 new Vue({
   router,
