369f3149f2428aa7af7a9da1a4f7efe0ef963787
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-17 11:45:12 -0300

    add DeviceInfo + icons launchers

--- src/App.vue
+++ src/App.vue
@@ -11,31 +11,27 @@ export default {
   components: {
     Header
   },
-  mounted () {
-    const { PushNotifications } = Plugins
-    console.log('Plugins -> ', Plugins)
-    console.log('Push Notification -> ', PushNotifications)
-    PushNotifications.register()
+  async mounted () {
+    const { PushNotifications, Device } = Plugins
+    const info = await Device.getInfo()
+    if (info.platform !== 'web') {
+      PushNotifications.register()
+      PushNotifications.addListener('registration', token => {
+        console.log('Push registration success, token: ' + token.value)
+      })
 
-    PushNotifications.addListener('registration', token => {
-      alert('Push registration success, token: ' + token.value)
-      console.log('Push registration success, token: ' + token.value)
-    })
+      PushNotifications.addListener('registrationError', error => {
+        console.log('Error on registration: ' + JSON.stringify(error))
+      })
 
-    PushNotifications.addListener('registrationError', error => {
-      alert('Error on registration: ' + JSON.stringify(error))
-      console.log('Error on registration: ' + JSON.stringify(error))
-    })
+      PushNotifications.addListener('pushNotificationReceived', notification => {
+        console.log('Error on registration: ' + JSON.stringify(notification))
+      })
 
-    PushNotifications.addListener('pushNotificationReceived', notification => {
-      alert('Push received: ' + JSON.stringify(notification))
-      console.log('Error on registration: ' + JSON.stringify(notification))
-    })
-
-    PushNotifications.addListener('pushNotificationActionPerformed', notification => {
-      alert('Push action performed: ' + JSON.stringify(notification))
-      console.log('Error on registration: ' + JSON.stringify(notification))
-    })
+      PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+        console.log('Error on registration: ' + JSON.stringify(notification))
+      })
+    }
   }
 }
 </script>
